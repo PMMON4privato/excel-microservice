aa10387637ca8055b4d88b34aba7b8dde045b9a7
Δημητρης Θεοδωροπουλος <jim@Demetress-MacBook-Pro.local>
2018-11-21 13:06:07 +0200

    add boolean support

---
+++ src/main/java/com/sysunite/microservice/excel/enum_type/CellDataTypeEnum.java
@@ -0,0 +1,19 @@
+package com.sysunite.microservice.excel.enum_type;
+
+public enum CellDataTypeEnum {
+
+  NUMBER("number"),
+  STRING("string"),
+  BOOLEAN("boolean"),
+  ERROR("error");
+
+  private String dataType;
+
+  CellDataTypeEnum(String dataType) {
+    this.dataType = dataType;
+  }
+
+  public String dataType() {
+    return this.dataType;
+  }
+}
